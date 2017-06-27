9cfdb5645c4732eabdeb53d2f7afbce90c1d63eb
Brian David Tafur Londoño <Brian.tafur@yuxiglobal.com>
2017-06-27 11:41:25 -0500

    first commit

---
+++ ExampleLogginDI/ExampleLogginDI/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using ExampleLogginDI.Services;
+
+namespace ExampleLogginDI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IMonsterService _monsterService;
+
+        public HomeController(IMonsterService monsterService)
+        {
+            _monsterService = monsterService;
+            _monsterService.Log("Haciendo una prueba del log....");
+            Console.WriteLine("Final del constructor");
+            
+        }
+
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = _monsterService.Log("nada");
+            return View();
+            
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExampleLogginDI/ExampleLogginDI/Services/IMonsterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ExampleLogginDI.Models;
+
+namespace ExampleLogginDI.Services
+{
+    public interface IMonsterService
+    {
+
+        String Log(Object msg);
+        void Error(Object msg, Exception e);
+
+    }
+}
