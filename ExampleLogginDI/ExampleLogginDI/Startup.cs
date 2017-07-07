f783df3aa152221d9644447ee5b69ac5e8f79b1d
Brian David Tafur Londoño <Brian.tafur@yuxiglobal.com>
2017-07-07 07:59:51 -0500

    Logging Test

--- ExampleLogginDI/ExampleLogginDI/Startup.cs
+++ ExampleLogginDI/ExampleLogginDI/Startup.cs
@@ -55,11 +55,11 @@ namespace ExampleLogginDI
             services.AddTransient<ISmsSender, AuthMessageSender>();
 
             //Add the Logger service
-            //var key = Convert.ToString(Configuration.GetSection("AzureKey").GetValue<String>("App_Insights_Logging_Key"));
-            //services.AddTransient<Logger.Services.LoggerInterface,
-            //Logger.Services.AppInsight>((_) => new Logger.Services.AppInsight(key));
+            var key = Convert.ToString(Configuration.GetSection("AzureKey").GetValue<String>("App_Insights_Logging_Key"));
+            services.AddTransient<Logger.Services.LoggerInterface,
+            Logger.Services.AppInsight>((_) => new Logger.Services.AppInsight(key));
             //services.AddTransient<Logger.Services.LoggerInterface, Logger.Services.Serilog>();
-            services.AddTransient<Logger.Services.LoggerInterface, Logger.Services.Log4Net>();
+            //services.AddTransient<Logger.Services.LoggerInterface, Logger.Services.Log4Net>();
 
         }
 
