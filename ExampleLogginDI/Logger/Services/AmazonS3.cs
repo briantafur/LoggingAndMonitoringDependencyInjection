4b6127c4d5df693c79f6082bbe33050ab13c5ebe
Brian David Tafur Londoño <briantafur89@hotmail.com>
2017-10-03 09:32:27 -0500

    adding amazon s3 storage

---
+++ ExampleLogginDI/Logger/Services/AmazonS3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Logger.Services
+{
+    class AmazonS3 : ILoggerInterface
+    {
+        public void Debug(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DebugAsync(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception exception, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ErrorAsync(Exception exception, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(Exception exception, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FatalAsync(Exception exception, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InfoAsync(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WarningAsync(string message, Type component, [CallerMemberName] string methodName = "")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
